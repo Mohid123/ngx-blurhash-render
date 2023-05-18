3721bf7c71b3f9129dee71988482f404337babf8
Mohid123 <muhammadmohid141@gmail.com>
2023-05-18 11:05:42 +0500

    Images can now be assigned custom border radius, width and height

--- workspace/projects/ngx-blurhash-render/src/lib/ngx-blurhash.component.ts
+++ workspace/projects/ngx-blurhash-render/src/lib/ngx-blurhash.component.ts
@@ -2,10 +2,20 @@ import { isPlatformBrowser } from '@angular/common';
 import { Component, ElementRef, Inject, Input, PLATFORM_ID, ViewChild } from '@angular/core';
 import { decode } from 'blurhash';
 
+/**
+ * Blurhash rendering component for Angular 15+
+ */
 @Component({
   selector: 'ngx-blurhash-render',
   template: `
-    <canvas #canvas [width]="canvasWidth" [height]="canvasHeight"></canvas>
+    <canvas
+      #canvas
+      [width]="canvasWidth"
+      [height]="canvasHeight"
+      [style.borderRadius]="borderRadius"
+      [style.width]="width"
+      [style.height]="height"
+    ></canvas>
     <img
       alt="blurred-image"
       [ngSrc]="imageSrc"
@@ -14,6 +24,9 @@ import { decode } from 'blurhash';
       (load)="imageLoad = true"
       (onloadeddata)="imageLoaded = true"
       [class.img-loaded]="imageLoad"
+      [style.borderRadius]="borderRadius"
+      [style.width]="width"
+      [style.height]="height"
     >
   `,
   styles: [
@@ -22,34 +35,32 @@ import { decode } from 'blurhash';
       display: block;
       position: relative;
     }
-    
+
     canvas {
       height: 100%;
       width: 100%;
       position: absolute;
       left: 0;
-      top: 0;
     }
-    
+
     img {
       opacity: 0;
-      width: inherit;
-      height: inherit;
+      width: 100%;
+      height: 100%;
       position: absolute;
       object-fit: cover;
       left: 0;
-      top:0
     }
-    
+
     .img-loaded {
       animation: popIn 0.4s both ease-in;
     }
-  
+
     @keyframes popIn {
       0% {
         opacity: 0;
       }
-    
+
       100% {
         opacity: 1;
       }
@@ -60,6 +71,10 @@ import { decode } from 'blurhash';
 export class NgxBlurhashComponent {
 
   private blurHashValue!: string;
+
+  /**
+   * The blurHash string to render on the Canvas
+   */
   @Input()
   get blurHash(): string {
     return this.blurHashValue;
@@ -69,6 +84,10 @@ export class NgxBlurhashComponent {
     this.decodeBlurHash();
   }
 
+
+  /**
+   * The image src string to render after fully loaded
+   */
   private imageSrcValue!: string;
   @Input()
   get imageSrc(): string {
@@ -78,9 +97,25 @@ export class NgxBlurhashComponent {
     this.imageSrcValue = value;
   }
 
+  /**
+   * The loading of the image based on its position in the DOM. By default loading is set to 'eager'
+   */
   @Input() loading: string = 'eager';
-  @Input() first: boolean = false;
 
+  /**
+   * Border Radius for the image in px. Default value is 0px
+   */
+  @Input() borderRadius: string = '0';
+
+  /**
+   * The width of the image and canvas in px. Default value is 200px
+   */
+  @Input() width: string = '200px';
+
+  /**
+   * The height of the image and canvas in px. Default value is 200px
+   */
+  @Input() height: string = '200px';
 
   public imageLoaded = false;
   public imageLoad = false;
